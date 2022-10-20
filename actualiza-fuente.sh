df95658e3836d90379cd0ccb94999e8b67525872
Nachec <nachopacheco@gmail.com>
2022-10-20 06:56:38 -0500

    Ajustes menores para mejorar el rendimiento

--- actualiza-fuente.sh
+++ actualiza-fuente.sh
@@ -3,15 +3,27 @@
 # Actualiza el directorio original en inglés
 echo
 echo Actualiza repositorio Content en inglés
+echo -------------------------------------------
+echo
 cd ../content
+
+git restore yarn.lock
+
 git pull origin main
+
+yarn install
 
 # Actualiza el directorio en español
 echo
 echo Actualiza el repositorio translated-content
+echo ----------------------------------------------
+echo
+
 cd ../translated-content
 git stash
 git pull origin main
 git stash pop
 git stash clear
-
+echo
+echo ¡Listo! 
+echo ----------------------------------------------
